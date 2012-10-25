00329a211f803922d37ab8395f9a6585c83512e0
Paul Redmond <paulrredmond@gmail.com>
2012-10-24 22:03:11 -0700

    Adding frontend logic to hide announcements.
    Currently depends on jQuery. Will be generic eventually.

---
+++ Resources/public/js/announcements.js
@@ -0,0 +1,22 @@
+function hideAnnouncement(announcement_created_at) {
+    createCookie(announcement_created_at, 'hidden', 365);
+    $("#announcement").slideUp();
+}
+
+// http://www.quirksmode.org/js/cookies.html
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+$(document).ready(function () {
+    $('#hide_announcement').click(function (e) {
+        e.preventDefault();
+        hideAnnouncement($(this).data('announcement'));
+    });
+});
